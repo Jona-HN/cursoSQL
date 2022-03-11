2002bafd37ee9dfab7947729f164c38d9ab90f63
Jonathan Herrera <msjona28@outlook.com>
2022-03-10 19:19:41 -0800

    Ejercicio 1. Nota: duda entre usar WHERE y HAVING

---
+++ Ejercicio1.sql
@@ -0,0 +1,19 @@
+# Ejercicio 1
+# Mostrar más información de las películas rentadas
+
+USE sakila;
+
+SELECT
+r.rental_id as id, 
+f.title as Título, 
+r.rental_date as "Fecha de renta",
+rental_rate as "Índice de renta",
+special_features as "Escenas especiales",
+rating as Calificación
+FROM rental r
+LEFT JOIN inventory i on r.inventory_id = i.inventory_id
+LEFT JOIN film f on i.film_id = f.film_id
+#WHERE rental_rate > 2
+GROUP BY f.title
+HAVING rental_rate > 2
+# Duda: ¿qué diferencia hay entre usar WHERE y HAVING?
